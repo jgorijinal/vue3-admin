57948b481b19a586b99939a2a3f4dd6874c0583f
jgoriginal <932274204@qq.com>
2022-09-18 14:36:38 +0800

    feat: 渲染用户头像菜单

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,7 @@
 const getters = {
   token: (state) => state.user.token,
+  userInfo: (state) => state.user.userInfo,
+  // 返回 true/ false , 判断有无用户资料
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   }
