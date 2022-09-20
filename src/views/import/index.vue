2c96333a0ca1e200b2242fe4ed6387cfa25120e1
jgoriginal <932274204@qq.com>
2022-09-20 22:57:09 +0800

    feat: 文件选择之后的数据解析处理

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="">
-    <upload></upload>
+    <upload :onSuccess="onSuccess"></upload>
   </div>
 </template>
 
 <script setup>
 import { } from 'vue'
 import Upload from '@/components/Upload/index.vue'
+const onSuccess = (excelData) => {
+  console.log(excelData)
+}
 </script>
 
 <style lang="scss" scoped></style>
