25b77c52314e01a6ac58416d2be5b47257878bdd
jgoriginal <932274204@qq.com>
2022-11-20 13:49:03 +0800

    feat: 创建对tagsViewList数据的快捷访问

---
+++ src/components/TagsView/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <template>
+
+    </template>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -8,7 +8,8 @@ const getters = {
   },
   cssVar: (state) => variables,
   sidebarOpened: (state) => state.app.sidebarOpened,
-  language: (state) => state.app.lang
+  language: (state) => state.app.lang,
+  tagsViewList: state => state.app.tagsViewList
 }
 
 export default getters
